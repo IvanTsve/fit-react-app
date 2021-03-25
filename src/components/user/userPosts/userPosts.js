088ad7ed9790fb71310e4b1bd510ca84abb64daa
unknown <iv_plamenovv@abv.bg>
2021-03-25 13:51:44 +0200

    added more tempolary style for user profile

--- src/components/user/userPosts/userPosts.js
+++ src/components/user/userPosts/userPosts.js
@@ -1,8 +1,20 @@
 import {Link} from 'react-router-dom';
+import './userPosts.css';
 
 function userPost(params) {
     return (
             <section className="user-posts">
+               <div className="post">
+               <img src="https://www.jobycodirect.com/images/items/9thscience.jpg" alt="profile picture" />
+               <p className="post-introduction">lorem ipsum lorem ipsumlorem ipsum</p>
+               <Link className="CTA-read" to="#">ReadMore</Link>
+               </div>
+               <div className="post">
+               <img src="https://www.jobycodirect.com/images/items/9thscience.jpg" alt="profile picture" />
+               <p className="post-introduction">lorem ipsum lorem ipsumlorem ipsum</p>
+               <Link className="CTA-read" to="#">ReadMore</Link>
+               </div>
+               
                <div className="post">
                <img src="https://www.jobycodirect.com/images/items/9thscience.jpg" alt="profile picture" />
                <p className="post-introduction">lorem ipsum lorem ipsumlorem ipsum</p>
