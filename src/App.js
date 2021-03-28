cbdcbdeafed5dd10b822f5425a0761d7d0988dda
unknown <iv_plamenovv@abv.bg>
2021-03-28 15:22:00 +0300

    add site footer

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import Header from './components/header/header';
 import Main from './components/main/main';
 import ContactUs from './components/contacts/contacts';
 import User from './components/user/user';
-
+import Footer from './components/footer/footer';
 
 function App() {
   return (
-      <div className="App">
-        <Header/>
-        <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/contact-us" exact component={ContactUs} />
-          <Route path="/user/profile" exact component={User} />
-          <Route path="/user/login" exact component={ContactUs} />
-          <Route path="/user/register" exact component={ContactUs} />
-          <Route path="/user/logout" exact component={ContactUs} />
-        </Switch>
-  
-      </div>
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/contact-us" exact component={ContactUs} />
+        <Route path="/user/profile" exact component={User} />
+        <Route path="/user/login" exact component={ContactUs} />
+        <Route path="/user/register" exact component={ContactUs} />
+        <Route path="/user/logout" exact component={ContactUs} />
+      </Switch>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import './footer.css';
+
+function footer() {
+    return (
+        <div>
+            <ul className="site-footer">
+                <li>INSTAGRAM</li>
+                <li>FACEBOOK</li>
+                <li>EMAIL</li>
+                <li>PHONE</li>
+            </ul>
+        </div>
+    )
+}
+
+export default footer;
